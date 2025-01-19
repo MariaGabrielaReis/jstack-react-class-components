2332d7784cd5a8ebb746e2d95d7ff4f23920aaaf
MariaGabrielaReis <mariagabrielagreis@gmail.com>
2025-01-18 23:26:46 -0300

    feat: use local storage to save user theme

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -3,7 +3,25 @@ import React, { createContext } from "react";
 export const ThemeContext = createContext();
 
 export class ThemeProvider extends React.Component {
-  state = { theme: "dark" };
+  constructor(props) {
+    super(props);
+
+    let theme = "dark";
+    try {
+      theme = JSON.parse(localStorage.getItem("@JStackClassComponents:theme"));
+    } catch (err) {
+      console.error(err);
+    }
+    this.state = { theme };
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.theme !== this.state.theme)
+      localStorage.setItem(
+        "@JStackClassComponents:theme",
+        JSON.stringify(this.state.theme),
+      );
+  }
 
   handleToggleTheme = () => {
     this.setState(prevState => ({
