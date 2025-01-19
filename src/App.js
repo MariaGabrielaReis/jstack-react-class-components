31e74c460b506f04785d6c23a4ae4f6b0896d5d8
MariaGabrielaReis <mariagabrielagreis@gmail.com>
2025-01-18 23:13:03 -0300

    feat: use life cycle class methods

--- src/App.js
+++ src/App.js
@@ -8,15 +8,47 @@ import GlobalStyle from "./styles/global";
 import themes from "./styles/themes";
 
 export default class App extends React.Component {
+  state = { changed: false };
+
+  componentDidMount() {
+    console.log("componentDidMount executed");
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("componentDidUpdate executed", {
+      currentState: this.state,
+      prevState,
+      prevProps,
+    });
+  }
+
+  componentDidCatch(error, info) {
+    console.log("componentDidCatch executed", { error, info });
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("shouldComponentUpdate executed", {
+      currentState: this.state,
+      nextState,
+      nextProps,
+    });
+
+    return true;
+  }
+
   render() {
+    console.log("rendered");
+
     return (
       <>
         <ThemeProvider>
           <ThemeContext.Consumer>
-            {({ theme }) => (
+            {({ theme, handleToggleTheme }) => (
               <StyledThemeProvider theme={themes[theme] || themes.dark}>
+                <button onClick={handleToggleTheme}>Change state</button>
+
                 <GlobalStyle />
-                <Layout />
+                {theme === "dark" && <Layout />}
               </StyledThemeProvider>
             )}
           </ThemeContext.Consumer>
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -4,12 +4,28 @@ import Footer from "../Footer";
 import Header from "../Header";
 import PostsList from "../PostsList";
 
-export default function Layout() {
-  return (
-    <>
-      <Header />
-      <PostsList />
-      <Footer />
-    </>
-  );
+export default class Layout extends React.Component {
+  componentDidMount() {
+    console.log("componentDidMount layout executed");
+    document.addEventListener("scroll", this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount layout executed");
+    document.removeEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll = () => {
+    console.log("scrolled...");
+  };
+
+  render() {
+    return (
+      <>
+        <Header />
+        <PostsList />
+        <Footer />
+      </>
+    );
+  }
 }
